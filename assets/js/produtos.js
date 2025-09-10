4a3784763a2e8fb0aa8fd2492bba6237d72f6e86
felipe correa <lipeaba13@gmail.com>
2025-09-10 20:56:26 -0300

    Criado tela de carrinho

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -55,7 +55,184 @@ function chamarCarrinho() {
 
     if (carrinho.style.display === "none") {
         carrinho.style.display = "flex";
+        let tabelaCarrinho = document.createElement("table");
+        tabelaCarrinho.classList.add("tabela-carrinho");
+        let tBody = document.createElement("tbody");
+        carrinho.appendChild(tabelaCarrinho);
+        tabelaCarrinho.appendChild(tBody);
+
+        if (quantidadeDF > 0) {
+            let tR = document.createElement("tr");
+            let tDI = document.createElement("td");
+            let tDD = document.createElement("td");
+            let tDV = document.createElement("td");
+            let tDQ = document.createElement("td");
+            let tDLX = document.createElement("td");
+
+            tBody.appendChild(tR);
+            let img = document.createElement("img");
+            img.src = "../assets/image/cachorro-filhote";
+            img.classList.add("img-carrinho");
+            tDI.appendChild(img);
+            tR.appendChild(tDI);
+
+            let descricao = document.createElement("p");
+            descricao.innerText = "Ração de 15,0kg para cães filhotes.";
+            descricao.classList.add("descricao-carrinho");
+            tDD.appendChild(descricao);
+            tR.appendChild(tDD)
+
+            let valor = document.createElement("p");
+            let calcularValor = quantidadeDF * 190.90;
+            calcularValor = calcularValor.toFixed(2);
+            calcularValor = calcularValor.toString();
+            valor.innerText = `R$ ${calcularValor}`;
+            tDV.appendChild(valor);
+            tR.appendChild(tDV);
+
+            let quantidade = document.createElement("p");
+            quantidade.innerText = quantidadeDF;
+            tDQ.appendChild(quantidade);
+            tR.appendChild(tDQ);
+
+            let remover = document.createElement("button");
+            remover.setAttribute("type", "button");
+            remover.innerText = "LX";
+            remover.addEventListener("click", removerCarrinho);
+            tDLX.appendChild(remover);
+            tR.appendChild(tDLX)
+        }
+        if (quantidadeDA > 0) {
+            let tR = document.createElement("tr");
+            let tDI = document.createElement("td");
+            let tDD = document.createElement("td");
+            let tDV = document.createElement("td");
+            let tDQ = document.createElement("td");
+            let tDLX = document.createElement("td");
+
+            tBody.appendChild(tR);
+            let img = document.createElement("img");
+            img.src = "../assets/image/cachorro-adulto.webp";
+            img.classList.add("img-carrinho");
+            tDI.appendChild(img);
+            tR.appendChild(tDI);
+
+            let descricao = document.createElement("p");
+            descricao.innerText = "Ração de 15,0kg para cães adultos.";
+            descricao.classList.add("descricao-carrinho");
+            tDD.appendChild(descricao);
+            tR.appendChild(tDD)
+
+            let valor = document.createElement("p");
+            let calcularValor = quantidadeDA * 179.90;
+            calcularValor = calcularValor.toFixed(2);
+            calcularValor = calcularValor.toString();
+            valor.innerText = `R$ ${calcularValor}`;
+            tDV.appendChild(valor);
+            tR.appendChild(tDV);
+
+            let quantidade = document.createElement("p");
+            quantidade.innerText = quantidadeDA;
+            tDQ.appendChild(quantidade);
+            tR.appendChild(tDQ)
+
+            let remover = document.createElement("button");
+            remover.setAttribute("type", "button");
+            remover.innerText = "LX";
+            remover.addEventListener("click", removerCarrinho);
+            tDLX.appendChild(remover);
+            tR.appendChild(tDLX)
+
+        }
+        if (quantidadeCF > 0) {
+            let tR = document.createElement("tr");
+            let tDI = document.createElement("td");
+            let tDD = document.createElement("td");
+            let tDV = document.createElement("td");
+            let tDQ = document.createElement("td");
+            let tDLX = document.createElement("td");
+
+            tBody.appendChild(tR);
+            let img = document.createElement("img");
+            img.src = "../assets/image/gato-filhote.jpg";
+            img.classList.add("img-carrinho");
+            tDI.appendChild(img);
+            tR.appendChild(tDI);
+
+            let descricao = document.createElement("p");
+            descricao.innerText = "Ração de 10,1kg para gatos filhotes.";
+            descricao.classList.add("descricao-carrinho");
+            tDD.appendChild(descricao);
+            tR.appendChild(tDD)
+
+            let valor = document.createElement("p");
+            let calcularValor = quantidadeCF * 139.90;
+            calcularValor = calcularValor.toFixed(2);
+            calcularValor = calcularValor.toString();
+            valor.innerText = `R$ ${calcularValor}`;
+            tDV.appendChild(valor);
+            tR.appendChild(tDV);
+
+            let quantidade = document.createElement("p");
+            quantidade.innerText = quantidadeCF;
+            tDQ.appendChild(quantidade);
+            tR.appendChild(tDQ)
+
+            let remover = document.createElement("button");
+            remover.setAttribute("type", "button");
+            remover.innerText = "LX";
+            remover.addEventListener("click", removerCarrinho);
+            tDLX.appendChild(remover);
+            tR.appendChild(tDLX)
+        }
+        if (quantidadeCA > 0) {
+            let tR = document.createElement("tr");
+            let tDI = document.createElement("td");
+            let tDD = document.createElement("td");
+            let tDV = document.createElement("td");
+            let tDQ = document.createElement("td");
+            let tDLX = document.createElement("td");
+
+            tBody.appendChild(tR);
+            let img = document.createElement("img");
+            img.src = "../assets/image/gato-adulto.jpg";
+            img.classList.add("img-carrinho");
+            tDI.appendChild(img);
+            tR.appendChild(tDI);
+
+            let descricao = document.createElement("p");
+            descricao.innerText = "Ração de 10,1kg para gatos adultos.";
+            descricao.classList.add("descricao-carrinho");
+            tDD.appendChild(descricao);
+            tR.appendChild(tDD)
+
+            let valor = document.createElement("p");
+            let calcularValor = quantidadeCA * 119.90;
+            calcularValor = calcularValor.toFixed(2);
+            calcularValor = calcularValor.toString();
+            valor.innerText = `R$ ${calcularValor}`;
+            tDV.appendChild(valor);
+            tR.appendChild(tDV);
+
+            let quantidade = document.createElement("p");
+            quantidade.innerText = quantidadeCA;
+            tDQ.appendChild(quantidade);
+            tR.appendChild(tDQ)
+
+            let remover = document.createElement("button");
+            remover.setAttribute("type", "button");
+            remover.innerText = "LX";
+            remover.addEventListener("click", removerCarrinho);
+            tDLX.appendChild(remover);
+            tR.appendChild(tDLX);
+        }
     } else {
         carrinho.style.display = "none";
+        carrinho.innerHTML = "";
     }
+
+}
+
+function removerCarrinho() {
+
 }
